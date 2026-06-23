34a72e20839222c374a765c55690ece1eca5c909
Vijayakrishnan_R <krishnanvijay802@gmail.com>
2026-06-23 13:31:56 +0530

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,27 @@
+package in.kce.bean;
+public class Employee {
+//instance variables should match with db table
+	private int empId;
+	private String empName;
+	private String designation;
+	
+	public int getEmpId() {
+		return empId;
+	}
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	public String getDesignation() {
+		return designation;
+	}
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+	
+}
